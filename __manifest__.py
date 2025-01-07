069c46f47c36e317d6d0397eda592ee1fa8271c0
LeoOrion <jpninanjohn@delium.co>
2025-01-07 15:56:34 +0530

    Add manifest for the odeosync app

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,12 @@
 {
-  "name": "Dcove Integration",
+  "name": "Odeo Sync",
   "author": "DeliumTech",
   "version": "1.0.0",
   "data": [
     "security/ir.model.access.csv",
+    "views/subscribe.xml",
     "views/sync.xml",
-    "views/menu.xml"
+    "views/env.xml"
+    "views/main_menu.xml"
   ]
 }
