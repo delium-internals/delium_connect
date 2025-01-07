ab165955dd3e49f679e01d3f2e3a7a2eb78dad12
LeoOrion <jpninanjohn@delium.co>
2025-01-07 16:03:05 +0530

    Add manifest for the odeosync app

--- __manifest__.py
+++ __manifest__.py
@@ -2,11 +2,12 @@
   "name": "Odeo Sync",
   "author": "DeliumTech",
   "version": "1.0.0",
+  'depends': ['base'],
   "data": [
     "security/ir.model.access.csv",
     "views/subscribe.xml",
     "views/sync.xml",
-    "views/env.xml"
+    "views/env.xml",
     "views/main_menu.xml"
   ]
 }
