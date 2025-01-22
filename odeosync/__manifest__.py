82e9bb138abfeec0e06860963cbf8129897e3846
LeoOrion <jpninanjohn@delium.co>
2025-01-22 11:51:38 +0530

    First description UI/UX

--- odeosync/__manifest__.py
+++ odeosync/__manifest__.py
@@ -14,9 +14,18 @@
   'license': 'LGPL-3',
   "data": [
     "security/ir.model.access.csv",
+    "views/css.xml",
     "views/subscribe.xml",
     "views/sync.xml",
     "views/env.xml",
     "views/main_menu.xml"
-  ]
+  ],
+  'assets': {
+    'web.assets_backend': [
+      'odeosync/static/description/styles/style.css',  # Add your CSS file here
+    ],
+    'web.assets_frontend': [
+      'odeosync/static/description/styles/style.css',  # Add your CSS file here
+    ]
+  }
 }
